96b6ae27c5e9c5c4b402ca9defd45ad67fc76a69
Mikhail Kononov <miklekononov@gmail.com>
2023-10-27 00:19:05 +0700

    add laravel fiiles

---
+++ app/Http/Controllers/ContriesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContriesController extends Controller
+{
+    public function create ():string {
+        //dd('test');die();
+        return 'test';
+    }
+}
